f7158354e5df13f0ba9d512ac4ed0693e6d65569
Wallace Erick <wallace_erick@hotmail.com>
2013-06-11 12:42:58 -0300

    Bug Label Colors

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,15 +3,13 @@ $(document).ready(function(){
 	//Checkboxes & Radio Buttons
 	$('input').iCheck({
 		//If you need, change the color!
-		checkboxClass: 'check red',
-		radioClass: 'radio red',
-		labelHover: true
+		checkboxClass: 'check orange',
+		radioClass: 'radio orange',
+		labelHover: true,
 	});
 
 	//Tabs
 	$.tabs();
-
-	$('input.input-error').prepend('add your smiley here');
 
 	//Not Required 
 	$('.checkboxes ul li').click(function(){ 

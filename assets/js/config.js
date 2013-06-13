0b62eb4bccdc2f604b6a43aebebd8780390d3527
Wallace Erick <wallace_erick@hotmail.com>
2013-06-13 14:14:15 -0300

    Finish Tooltips

--- assets/js/config.js
+++ assets/js/config.js
@@ -20,6 +20,16 @@ $(document).ready(function(){
 	//Select
 	$('select').selectbox();
 
+	//Tooltip
+	$('.north').tipsy({gravity: 'n'});
+    $('.south').tipsy({gravity: 's'});
+    $('.east').tipsy({gravity: 'e'});
+    $('.west').tipsy({gravity: 'w'});
+    $('.north-west').tipsy({gravity: 'nw'});
+    $('.north-east').tipsy({gravity: 'ne'});
+    $('.south-west').tipsy({gravity: 'sw'});
+    $('.south-east').tipsy({gravity: 'se'});
+
 	//Masked Inputs
 	$(".mask-data").mask("99/99/9999");
 	$(".mask-telefone").mask("(99) 9999-9999");

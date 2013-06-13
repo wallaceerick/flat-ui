58e70f9ff86fb460ac00a3d972d6e7ff220bdfba
Wallace Erick <wallace_erick@hotmail.com>
2013-06-13 13:49:28 -0300

    Finish Input Increment

---
+++ assets/js/jquery.increment.js
@@ -0,0 +1,25 @@
+$(function() {
+
+  $(".input-increment").append('<i class="button more">+</i><i class="button less">-</i>');
+
+  $(".button").on("click", function() {
+
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();
+
+    if ($button.text() == "+") {
+  	  var newVal = parseFloat(oldValue) + 1;
+  	} else {
+	   // Don't allow decrementing below zero
+      if (oldValue > 0) {
+        var newVal = parseFloat(oldValue) - 1;
+	    } else {
+        newVal = 0;
+      }
+	  }
+
+    $button.parent().find("input").val(newVal);
+
+  });
+
+});
